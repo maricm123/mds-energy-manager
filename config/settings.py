0d30f1c999bbc48b269a4fd399d718cd40ac47b7
mmaric123 <mihailomaric001@gmail.com>
2026-02-24 11:52:51 +0100

    added drf spectacular for swagger and api doc

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "corsheaders",
     "rest_framework",
+    "drf_spectacular",
     "rack",
     "device"
 ]
@@ -110,3 +111,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "MDS Energy Manager API",
+    "DESCRIPTION": "API documentation for MDS Energy Manager",
+    "VERSION": "1.0.0",
+}
--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 
     # API URL
     path("api-mds/", include("apis.api_mds.urls", namespace="api-mds")),
+    path("api-mds/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api-mds/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api-mds/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
